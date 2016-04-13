47ce0038bfb23513ff74c19542a547c76f750025
piotut <piotr.tutak@stxnext.pl>
2016-04-13 14:00:00 +0100

    Negative trend

--- nl4.c
+++ nl4.c
@@ -29,5 +29,73 @@ int main()
 		}
 	}
 
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
   return 0;
 }
