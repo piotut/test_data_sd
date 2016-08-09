e6d29cbc4911e8d38762d3588f1d4f9c650641c5
piotut <piotr.tutak@stxnext.pl>
2016-08-09 09:57:45 +0200

    add content of files

--- __very_veryverylong__file_name_with_weird_characters+_s-76^5%4d_][}{:.cpp
+++ __very_veryverylong__file_name_with_weird_characters+_s-76^5%4d_][}{:.cpp
@@ -1,1 +1,101 @@
-weird characters in filename
+//C hello world example
+#include <stdio.h>
+ 
+int main()
+{
+  int a = 5;
+  int b = 6;
+  int c = 6;
+  int d = 7;
+  int e = 9;
+  int f = 15;
+
+  int result;
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  if(a>4){
+		if(b>3){
+			if(c>6){
+				result = a + b;
+				result = c + a;
+				if(d>7){
+					if(e>8){
+						if(f>10){
+							result = a + b + c + d + e + f;
+							printf("%d", result);
+						}
+					}
+				}
+			}
+		}
+	}
+
+  return 0;
+}
